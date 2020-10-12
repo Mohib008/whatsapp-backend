4834bd7fce852352e3c66e6efdecc7a84f3cea0a
Mohibullah Arsala <mohib-arsala@Mohib-MacBook-Pro.local>
2020-10-12 06:24:54 -0400

    pusher added for mongodb

---
+++ dbMessages.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+
+const whatsappSchema = mongoose.Schema({
+    message: String,
+    name: String,
+    timestamp: String,
+    recieved: Boolean
+});
+
+// msg Collections. 
+
+export default mongoose.model("messagecontent", whatsappSchema);
--- server.js
+++ server.js
@@ -1,13 +1,71 @@
 import express from "express";
+import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
+import Pusher from 'pusher';
+
+
+// App config
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 3000;
+
+const pusher = new Pusher({
+    appId: '1088700',
+    key: '34934fb3179b91572f8d',
+    secret: '5ebc3f4b77bd9c0193e5',
+    cluster: 'us2',
+    encrypted: true
+});
+
+
+// middleware
+
+app.use(express.json());
+
+// DB config
+
+const connection_url = "mongodb+srv://Arsala:wwc41FckwBYvKrDE@cluster0.7xmjw.mongodb.net/whatsappdb?retryWrites=true&w=majority"
+
+mongoose.connect(connection_url,{
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.once('open', () => {
+    console.log("Db is connected!");
+    const msgCollection = db.collection('messagecontents');
+    const changeStream = msgCollection.watch();
+});
+
+
+//Api route 
+
+app.get('/', (req, res) => res.status(200).send("Hellow world, this is from server.js"));
+
+app.get("/messages/sync", (req, res) => {
+    Messages.find((err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    })
+})
+
+app.post('/messages/new', (req, res) => {
+    const dbMessage = req.body;
+
+    Messages.create(dbMessage, (err, data) => {
+        if(err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(`new message created: \n ${data}`)
+        }
+    })
+})
 
 
 
-app.get('/', (req, res) => res.status(200).send("Hellow world"));
 
-
-
-
-
-app.listen(port, () => console.log(`App is running in Port 9000:${port}`));
+app.listen(port, () => console.log(`App is running in localhost:${port}`));
